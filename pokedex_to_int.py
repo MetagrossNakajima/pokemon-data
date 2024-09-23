0ed7a391d145739f79c300e8b8efd2c1f10ebea4
Metagross Nakajima <teru199798@gmail.com>
2024-09-23 18:29:48 +0900

    pokedexをintに修正

---
+++ pokedex_to_int.py
@@ -0,0 +1,11 @@
+from utils.json_utils import load_json, save_json
+
+pokemons = load_json("./v1/pokemons.json")
+result = {}
+
+
+for key, value in pokemons.items():
+    result[key] = value
+    result[key]["pokedex"] = int(value["pokedex"])
+
+save_json("./v1/pokemons.json", result)
